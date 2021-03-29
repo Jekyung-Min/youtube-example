cfdfc1c81809bcee2ff79f9271c17b62bdc05ce4
Jekyung-Min <cwm7485@naver.com>
2021-03-29 19:16:12 +0900

    added videos

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
 // import React from 'react';
-import './app.css';
+import { useState, useEffect } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello</h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCJaFHs1x7yhzYPzscUXCv4D5Hq2okk794"
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items));
+  }, []);
+
+  // console.log(videos);
+  return <VideoList videos={videos}></VideoList>;
 }
 
 export default App;
