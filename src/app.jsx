6b7bc66086b07b3a8991ef73676c7a4df2e56904
Jekyung-Min <cwm7485@naver.com>
2021-03-30 17:01:32 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,23 @@
-// import React from 'react';
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello</h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCJaFHs1x7yhzYPzscUXCv4D5Hq2okk794",
+      { method: "GET", redirect: "follow" }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setVideos(result.items);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos}></VideoList>;
 }
 
 export default App;
